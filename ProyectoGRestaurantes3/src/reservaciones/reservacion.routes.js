ec364e4796f7029f518527819fc665851fb5bb7c
fpineda-2021274 <fpineda-2021274@kinal.edu.gt>
2026-03-01 14:42:22 -0600

    fix: modificando routes  de entidad Reservaciones

--- ProyectoGRestaurantes3/src/reservaciones/reservacion.routes.js
+++ ProyectoGRestaurantes3/src/reservaciones/reservacion.routes.js
@@ -1,15 +1,23 @@
 import { Router } from 'express';
 import {
-    listarReservaciones, crearReservacion,
-    listarEstadosReservacion, crearEstadoReservacion
+    listarReservaciones, obtenerReservacion, crearReservacion, actualizarReservacion, eliminarReservacion,
+    listarEstadosReservacion, obtenerEstadoReservacion, crearEstadoReservacion, actualizarEstadoReservacion, eliminarEstadoReservacion
 } from './reservacion.controller.js';
 
 const router = Router();
 
+// RESERVACIONES
 router.get('/reservaciones', listarReservaciones);
+router.get('/reservaciones/:id', obtenerReservacion);
 router.post('/reservaciones', crearReservacion);
+router.put('/reservaciones/:id', actualizarReservacion);
+router.delete('/reservaciones/:id', eliminarReservacion);
 
+// ESTADOS DE RESERVACIÓN
 router.get('/estados-reservacion', listarEstadosReservacion);
+router.get('/estados-reservacion/:id', obtenerEstadoReservacion);
 router.post('/estados-reservacion', crearEstadoReservacion);
+router.put('/estados-reservacion/:id', actualizarEstadoReservacion);
+router.delete('/estados-reservacion/:id', eliminarEstadoReservacion);
 
 export default router;
