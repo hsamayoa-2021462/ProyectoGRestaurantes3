e5ab29d8c142c00e94fe32a93ddd977b1529934f
jregil-2021564 <jregil-2021564@kinal.edu.gt>
2026-05-01 11:21:53 -0600

    fix: modificando el correo del admin

--- ProyectoGRestaurantes3/ms-auth/helpers/admin-seed.js
+++ ProyectoGRestaurantes3/ms-auth/helpers/admin-seed.js
@@ -5,7 +5,7 @@ import { Role, UserRole } from '../src/roles/role.model.js';
 
 export const ensureAdminUser = async () => {
     try {
-        const adminEmail = 'kirsoalejo@gmail.com';
+        const adminEmail = 'arochajose775@gmail.com';
 
         // Buscar el usuario por email usando Email (PascalCase)
         const user = await User.findOne({
