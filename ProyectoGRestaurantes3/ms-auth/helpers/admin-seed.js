8596f10c1678263eaa01b8296bf89dca0a329396
aarocha-2024203 <aarocha-2024203@kinal.edu.gt>
2026-05-18 18:12:21 -0600

    fix: Se modifico el archivo admin-seed

--- ProyectoGRestaurantes3/ms-auth/helpers/admin-seed.js
+++ ProyectoGRestaurantes3/ms-auth/helpers/admin-seed.js
@@ -5,7 +5,7 @@ import { Role, UserRole } from '../src/roles/role.model.js';
 
 export const ensureAdminUser = async () => {
     try {
-        const adminEmail = 'samayoapaula716@gmail.com';
+        const adminEmail = 'alejandroarochavirula@gmail.com';
 
         // Buscar el usuario por email usando Email (PascalCase)
         const user = await User.findOne({
