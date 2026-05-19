eebc8ea2fb43e89456dae667ca028e2d5258b729
aarocha-2024203 <aarocha-2024203@kinal.edu.gt>
2026-05-18 18:26:56 -0600

    fix: Se modifico el archivo AppRoutes del fronted

--- client-admin-react/src/app/router/AppRoutes.jsx
+++ client-admin-react/src/app/router/AppRoutes.jsx
@@ -15,6 +15,8 @@ import AdminReservaciones from '../../features/admin/pages/AdminReservaciones'
 import AdminRestaurantes from '../../features/admin/pages/AdminRestaurantes'
 import AdminReportes from '../../features/admin/pages/AdminReportes'
 import AdminPerfil from '../../features/admin/pages/AdminPerfil'
+import AdminClientes from '../../features/admin/pages/AdminClientes'
+import AdminResenas from '../../features/admin/pages/AdminResenas'
 
 // Cliente pages
 import ClienteInicio from '../../features/cliente/pages/ClienteInicio'
@@ -22,6 +24,8 @@ import ClienteMenu from '../../features/cliente/pages/ClienteMenu'
 import ClienteMisPedidos from '../../features/cliente/pages/ClienteMisPedidos'
 import ClientePedido from '../../features/cliente/pages/ClientePedido'
 import ClienteReservar from '../../features/cliente/pages/ClienteReservar'
+import ClienteMisReservaciones from '../../features/cliente/pages/ClienteMisReservaciones'
+import ClienteResenas from '../../features/cliente/pages/ClienteResenas'
 import ClientePerfil from '../../features/cliente/pages/ClientePerfil'
 
 export default function AppRoutes() {
@@ -33,12 +37,10 @@ export default function AppRoutes() {
 
   return (
     <Routes>
-      {/* Rutas públicas */}
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/verify-email" element={<VerifyEmailPage />} />
       <Route path="/reset-password" element={<ResetPasswordPage />} />
 
-      {/* Rutas protegidas ADMIN */}
       <Route element={<ProtectedRoute allowedRoles={['admin']} />}>
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/menu" element={<AdminMenu />} />
@@ -47,19 +49,21 @@ export default function AppRoutes() {
         <Route path="/admin/restaurantes" element={<AdminRestaurantes />} />
         <Route path="/admin/reportes" element={<AdminReportes />} />
         <Route path="/admin/perfil" element={<AdminPerfil />} />
+        <Route path="/admin/clientes" element={<AdminClientes />} />
+        <Route path="/admin/resenas" element={<AdminResenas />} />
       </Route>
 
-      {/* Rutas protegidas CLIENTE */}
       <Route element={<ProtectedRoute allowedRoles={['cliente']} />}>
         <Route path="/cliente/inicio" element={<ClienteInicio />} />
         <Route path="/cliente/menu" element={<ClienteMenu />} />
         <Route path="/cliente/mis-pedidos" element={<ClienteMisPedidos />} />
         <Route path="/cliente/pedido/:id" element={<ClientePedido />} />
         <Route path="/cliente/reservar" element={<ClienteReservar />} />
+        <Route path="/cliente/mis-reservaciones" element={<ClienteMisReservaciones />} />
+        <Route path="/cliente/resenas" element={<ClienteResenas />} />
         <Route path="/cliente/perfil" element={<ClientePerfil />} />
       </Route>
 
-      {/* Raíz: redirige según rol */}
       <Route
         path="/"
         element={
@@ -70,8 +74,6 @@ export default function AppRoutes() {
               : <Navigate to="/cliente/inicio" replace />
         }
       />
-
-      {/* 404 */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
